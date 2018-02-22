9aa28cf617dadd638d29e184ab99876f3424ed47
georob223 <robbie.gascoigne@gmail.com>
2018-02-21 19:27:19 -0600

    Added global classes for car variables and for buyer info.

---
+++ Globals.py
@@ -0,0 +1,20 @@
+class Car():
+    def __init__(self):
+        self.paint=""
+        self.options={}
+        self.total_cost=0
+        self.model=""
+        self.power=""
+        self.extra_options={}
+
+class Buyer():
+    def __init__(self):
+        self.address=""
+        self.town=""
+        self.state_prov=""
+        self.country=""
+        self.zip=""
+        self.name=""
+        self.phone=""
+        self.email=""
+
